93df823a99c1b612783dc6c7a4f8a69450150d65
open0072023 <124534410+open0072023@users.noreply.github.com>
2023-03-07 16:51:53 +0800

    Add files via upload

---
+++ setconfigfour.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp MSG1500four.config /opt/padavan/trunk/configs/templates/MSG1500.config
+cp board.h /opt/padavan/trunk/configs/boards/MSG1500/board.h
+cp build_firmware_modifyfour /opt/padavan/trunk/build_firmware_modify
---
+++ setconfigkvr.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp MSG1500-7615kvr.config /opt/padavan/trunk/configs/templates/MSG1500-7615.config
+cp board.h /opt/padavan/trunk/configs/boards/MSG1500-7615/board.h
+cp build_firmware_modifykvr /opt/padavan/trunk/build_firmware_modify
